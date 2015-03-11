ce893bb9dae0b642c4ae3f1239a6e82ab53c14e3
Ann Millspaugh <amillspaugh@fool.com>
2015-03-11 16:17:20 -0400

    refactor to class-based views

--- finance/urls.py
+++ finance/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from finance.views import school_detail, school_list
+from rest_framework.urlpatterns import format_suffix_patterns
+from finance import views
 
 urlpatterns = [
-    url(r'^schools/$', school_list),
-    url(r'^schools/(?P<pk>[0-9]+)/$', school_detail),
+    url(r'^schools/$', views.SchoolList.as_view()),
+    url(r'^schools/(?P<pk>[0-9]+)/$', views.SchoolDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- finance/views.py
+++ finance/views.py
@@ -1,53 +1,51 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
 from finance.models import School
 from finance.serializers import SchoolSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
-@api_view(['GET', 'POST'])
-def school_list(request):
+class SchoolList(APIView):
     """
     List all schools, or create a new school.
     """
-    if request.method == 'GET':
-        schools = School.objects.all()
-        serializer = SchoolSerializer(schools, many=True)
+    def get(self, request, format=None):
+        school = School.objects.all()
+        serializer = SchoolSerializer(school, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = SchoolSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def school_detail(request, pk):
+class SchoolDetail(APIView):
     """
-    Retrieve, update or delete a school.
+    Retrieve, update or delete a school instance.
     """
-    try:
-        school = School.objects.get(pk=pk)
-    except School.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    def get_object(self, pk):
+        try:
+            return School.objects.get(pk=pk)
+        except School.DoesNotExist:
+            raise Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        school = self.get_object(pk)
         serializer = SchoolSerializer(school)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        school = self.get_object(pk)
         serializer = SchoolSerializer(school, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        school = self.get_object(pk)
         school.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
