99ddc41d46ee497c7a7809cbef14591e2026d173
Ann Millspaugh <amillspaugh@fool.com>
2015-03-19 19:16:37 -0400

    db

--- config/live.py
+++ config/live.py
@@ -2,16 +2,16 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 import dj_database_url
 
-# DATABASES = {
-#     'default':  {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'edu_api',
-#         'USER': 'amillspaugh',
-#         'PASSWORD': 'knew1for!',
-#         'HOST': 'dept-of-edu.cvqqp54rm4vr.us-east-1.rds.amazonaws.com',
-#         'PORT': 3306,
-#     }
-# }
+DATABASES = {
+    'default':  {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'edu_api',
+        'USER': 'amillspaugh',
+        'PASSWORD': 'knew1for!',
+        'HOST': 'dept-of-edu.cvqqp54rm4vr.us-east-1.rds.amazonaws.com',
+        'PORT': 3306,
+    }
+}
 
 
 DATABASES['default'] =  dj_database_url.config()
