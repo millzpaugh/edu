2bebd3b1ebc1e319bdfd2e74a6a4a76717aae96c
Ann Millspaugh <amillspaugh@fool.com>
2015-03-19 18:31:25 -0400

    add settings

--- edu/wsgi.py
+++ edu/wsgi.py
@@ -14,9 +14,12 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 # application = get_wsgi_application()
 
 import os
-import sys
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edu.settings")
+
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
+
 application = Cling(get_wsgi_application())
+
