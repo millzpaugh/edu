acc7ebccb16d75f293ffd90d9e253aac059d4f29
Ann Millspaugh <amillspaugh@fool.com>
2015-03-23 12:31:21 -0400

    add back config

---
+++ config/dev.py
@@ -0,0 +1,27 @@
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'edu',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
+#     }
+# }
+
+DATABASES = {
+    'default':  {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'edu_api',
+        'USER': 'ebroot',
+        'PASSWORD': 'knew1for!',
+        'HOST': 'dept-of-edu.cvqqp54rm4vr.us-east-1.rds.amazonaws.com',
+        'PORT': 3306,
+    }
+}
